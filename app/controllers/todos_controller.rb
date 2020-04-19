20389377c1dcd83d04e6e2e9cc170d08b2fda254
SrinivasaSaran <121004244@sastra.ac.in>
2020-04-19 14:09:19 +0530

    Render list of todos from todos table

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,7 @@
 class TodosController < ApplicationController
   def index
-    render plain: "Hello Rails! Here we Go.. BOOM ~_~ #{DateTime.now} \nHello, this is /todos! #{DateTime.now.to_s(:short)}"
+    render plain: "Hello Rails! Here we Go.. BOOM ~_~ #{DateTime.now}
+    \nHello, this is /todos! #{DateTime.now.to_s(:short)}
+    \n\nLets see whats in todos table,\n\n #{Todo.all.to_a}"
   end
 end
