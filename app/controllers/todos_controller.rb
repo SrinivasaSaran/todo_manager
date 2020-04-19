298de490cfa0fbac2394cee9673187828496be6a
SrinivasaSaran <121004244@sastra.ac.in>
2020-04-19 14:38:33 +0530

    Render list of formated, neat_looking todos from todos table

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,6 +2,10 @@ class TodosController < ApplicationController
   def index
     render plain: "Hello Rails! Here we Go.. BOOM ~_~ #{DateTime.now}
     \nHello, this is /todos! #{DateTime.now.to_s(:short)}
-    \n\nLets see whats in todos table,\n\n #{Todo.all.to_a}"
+    \n\nLets see whats in todos table,\n\n #{Todo.all.to_a}
+    \n\nTo neat looking text,
+    \n#{Todo.all.map { |todo| todo.to_neat_look }.join("\n")}
+    \n\nDate-wise ordered todo,
+    \n#{Todo.order(:due_date).map { |todo| todo.to_neat_look }.join("\n")}"
   end
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,6 @@
 class Todo < ActiveRecord::Base
+  def to_neat_look
+    is_complete = completed ? "[X]" : "[ ]"
+    "#{id}. Due: #{due_date.to_s(:rfc822)} #{todo_text} #{is_complete}"
+  end
 end
