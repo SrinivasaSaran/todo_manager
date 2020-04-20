258920734c40cf00d3efbed7f6a15799146d9e39
SrinivasaSaran <121004244@sastra.ac.in>
2020-04-20 14:47:18 +0530

    ff  created new table 'users' using rails migration. Also routes for users are added. All required ruby files for user management are created with skeletan

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def login
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   #get "todos/:id", to: "todos#show"
 
   resources :todos
+  resources :users
+  post "users/login", to: "users#login"
 end
