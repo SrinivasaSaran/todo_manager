1bd98961df32ed56369e0a7796c63529453c42c3
SrinivasaSaran <121004244@sastra.ac.in>
2020-05-04 01:54:43 +0530

    Level-7 work done. Completed todos, which aren't overdue, will now appear checked and striked out

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -24,6 +24,10 @@ class Todo < ActiveRecord::Base
     where(completed: false)
   end
 
+  def completed?
+    completed == true
+  end
+
   def self.add_task(new_task)
     create!(todo_text: new_task[:todo_text], due_date: Date.today + new_task[:due_in_days], completed: false)
   end
