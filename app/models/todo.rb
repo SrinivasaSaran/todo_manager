6d6160b9f225d2c05e2d3cea2ee99576aaf1710c
SrinivasaSaran <121004244@sastra.ac.in>
2020-05-03 20:35:23 +0530

    ERB templating is used to make the available static HTML page to dynamic page and learned about partials as well

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,4 +3,31 @@ class Todo < ActiveRecord::Base
     is_complete = completed ? "[X]" : "[ ]"
     "#{id}. Due: #{due_date.to_s(:rfc822)} #{todo_text} #{is_complete}"
   end
+
+  def self.overdue
+    where("due_date < ?", Date.today)
+  end
+
+  def self.due_today
+    where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    where("due_date > ?", Date.today)
+  end
+
+  def self.completed
+    where(completed: true)
+  end
+
+  def self.add_task(new_task)
+    create!(todo_text: new_task[:todo_text], due_date: Date.today + new_task[:due_in_days], completed: false)
+  end
+
+  def self.mark_as_complete!(completed_id)
+    todo = find(completed_id)
+    todo.completed = true
+    todo.save
+    todo
+  end
 end
