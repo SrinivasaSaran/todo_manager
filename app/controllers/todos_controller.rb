7186183e182d2f3e20e53d836fd3b8a329ffe79e
SrinivasaSaran <121004244@sastra.ac.in>
2020-05-05 14:12:32 +0530

    Used HTML form to add a new todo to db and display it immediately on adding

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,22 +2,11 @@ class TodosController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-=begin
-    render plain: "Hello Rails! Here we Go.. BOOM ~_~ #{DateTime.now}
-    \nHello, this is /todos! #{DateTime.now.to_s(:short)}
-    \n\nLets see whats in todos table,\n\n #{Todo.all.to_a}
-    \n\nTo neat looking text,
-    \n#{Todo.all.map { |todo| todo.to_neat_look }.join("\n")}
-    \n\nDate-wise ordered todo,
-    \n#{Todo.order(:due_date).map { |todo| todo.to_neat_look }.join("\n")}"
-=end
-    # render plain: Todo.order(:due_date).map { |todo| todo.to_neat_look }.join("\n")
     render "index"
   end
 
   def show
     id = params[:id]
-    #render plain: " #{Todo.find(id).to_neat_look}"
     render "show"
   end
 
@@ -25,8 +14,7 @@ class TodosController < ApplicationController
     new_todo = Todo.create!(todo_text: params[:todo_text],
                             due_date: DateTime.parse(params[:due_date]),
                             completed: false)
-    response_text = "New todo is created. Its id is #{new_todo.id}"
-    render plain: "#{response_text}"
+    redirect_to todos_path
   end
 
   def update
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,8 +1,4 @@
 class Todo < ActiveRecord::Base
-  def to_neat_look
-    is_complete = completed ? "[X]" : "[ ]"
-    "#{id}. Due: #{due_date.to_s(:rfc822)} #{todo_text} #{is_complete}"
-  end
 
   def self.overdue
     where("due_date < ?", Date.today)
@@ -27,15 +23,5 @@ class Todo < ActiveRecord::Base
   def completed?
     completed == true
   end
-
-  def self.add_task(new_task)
-    create!(todo_text: new_task[:todo_text], due_date: Date.today + new_task[:due_in_days], completed: false)
-  end
-
-  def self.mark_as_complete!(completed_id)
-    todo = find(completed_id)
-    todo.completed = true
-    todo.save
-    todo
-  end
+  
 end
