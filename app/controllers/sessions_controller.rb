e02709eb069d1ef9346ae832036002413b4e53ed
SrinivasaSaran <121004244@sastra.ac.in>
2020-05-10 20:18:39 +0530

    Session creation on user signin, made adjustment to routes, blocked unauthorised visit of main routes and few HTML modifications

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,11 @@
 class SessionsController < ApplicationController
+  skip_before_action :ensure_user_logged_in
+
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password_digest])
-      render plain: "You are now signed in"
+      session[:current_user_id] = user.id
+      redirect_to "/"
     else
       render plain: "Incorrect Mail or Password"
     end
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,6 @@
 class TodosController < ApplicationController
   def index
+    @user = current_user
   end
 
   def show
